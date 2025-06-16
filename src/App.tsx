82d5dbfefba1885da74010ce98a6df209a8debdd
juniper-j <jy.juniper.jeong@gmail.com>
2025-06-16 21:28:42 +0900

    ✨ feat: add Post page with tabbed interface for writing and generating text; update App component to render Post

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/Home'
+import Post from './pages/Post'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
@@ -17,7 +17,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Home />
+    <Post />
   )
 }
 
